2ec44a24557eddad1917abb774b176e177a6f724
Leylammmdl <akuma11tokito@gmail.com>
2025-04-29 20:50:17 +0400

    indi yazdım

---
+++ qaligin bilinmesi.js
@@ -0,0 +1,7 @@
+let num = 12;
+
+let power = num ** 5;
+let remainder = power % 3;
+
+console.log("Ədədin 5-ci dərəcə qüvvəti: " + power);
+console.log("Bu ədədin 3-ə bölünməsindən qalan: " + remainder);
